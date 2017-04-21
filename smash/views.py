9d4d741f16b8b775a00bbbb4201050299781cfee
SecretlyTaco <secretlytaco@gmail.com>
2017-04-21 02:21:06 +0100

    Added permalink to random quote

--- smash/views.py
+++ smash/views.py
@@ -44,6 +44,7 @@ def index():
     if len(quotes)>0:
         quote = random.choice(quotes)
         news = str(Markup.escape(quote.content)).replace('\n', '<br />')
+        news += str("<a href='quote/" + str(quote) + "'>Permalink</a>")
 
     return render_template(
         "index.html",
